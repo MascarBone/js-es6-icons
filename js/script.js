cd88490f67fe1d1dea8f27d4f1e23cf6172c3417
MascarBone <simone.bonelli@gmail.com>
2021-09-13 15:25:29 +0200

    printOnScreen created

--- js/script.js
+++ js/script.js
@@ -106,3 +106,47 @@ const icons = [
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
+/**
+ * Funzione per scrivere in HTML il contenuto
+ */
+
+printOnScreen(icons);
+
+let newTag = document.createElement("div");
+
+
+
+function printOnScreen (array){
+    // Mi salvo il riferimento al codice HTML
+    const content = document.querySelector('#my_content');
+
+    // innerHTML = "" in modo che ogni volta che chiamo la funzione resetta la pagina per non creare più volte lo stesso codice HTML
+    content.innerHTML = "";
+    
+    // Ciclo For Each che stampa nel codice per ogni indice dell'array
+    array.forEach(element => {
+        // Destrutturazione per estrapolare i valori di ogni elemento che cicla
+        const {family,prefix,name,type} = element;
+        // console.log(content);
+
+        // Aggiunta della riga di codice per creare l'html
+
+        // ########## Per usare append() ##########
+        // let tag = document.createElement("i");
+        // tag.classList.add(`${family}`);
+        // tag.classList.add(`${prefix}${name}`);
+        // console.log(tag);
+        // content.append(tag);
+        
+        content.innerHTML += `
+        <div class="col">
+            <i class="${family} ${prefix}${name}"></i>
+            <h4 class="text-uppercase">${name}</h4>
+        </div>
+        `
+    });
+}
+
+/**
+ * 
+ */
